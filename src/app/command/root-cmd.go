d7800e07c97a671ece6c4b509e482e470fcac23e
plastikfan <plastikfan@outlook.com>
2022-08-01 16:07:10 +0100

    ref: replace global state with bootstrap (#17)
    ref: move i18n into new internal package translate (#17)
    
    ref: define a parameter set for root command (#17)
    
    ref: replace init with bootstrap (#17)
    
    ref: remove global state from widget command (#17)

--- src/app/command/root-cmd.go
+++ src/app/command/root-cmd.go
@@ -6,93 +6,67 @@ package command
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/snivilised/cobrass/src/assistant"
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+	"golang.org/x/text/language"
 )
 
 const AppEmoji = "🦄"
 const ApplicationName = "arcadia"
 
-var cfgFile string
-var lang string
-
-var Container = assistant.NewCobraContainer(
-	&cobra.Command{
-		Use:   "main",
-		Short: "A brief description of your application",
-		Long: `A longer description that spans multiple lines and likely contains
-	examples and usage of using your application. For example:
-	
-	Cobra is a CLI library for Go that empowers applications.
-	This application is a tool to generate the needed files
-	to quickly create a Cobra application.`,
-		Version: fmt.Sprintf("'%v'", Version),
-		// Uncomment the following line if your bare application
-		// has an action associated with it:
-		// Run: func(cmd *cobra.Command, args []string) { },
-	},
-)
-var rootCommand = Container.Root()
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCommand.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
+	bs := bootstrap{}
+	bs.execute()
 }
 
-func init() {
-	cobra.OnInitialize(initConfig)
+type RootParameterSet struct {
+	ConfigFile string
+	Language   string
+	Toggle     bool
+}
 
+func setupRootCommand(container *assistant.CobraContainer) {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	bindToConfig := &cfgFile
 	const configFlagName = "config"
 	const defConfig = ""
-	const configUsage = "config file (default is $HOME/.arcadia.yml)"
-	rootCommand.PersistentFlags().StringVar(bindToConfig, configFlagName, defConfig, configUsage)
+	configUsage := fmt.Sprintf("config file (default is $HOME/.%v.yml", ApplicationName)
 
-	bindToLang := &lang
+	root := container.Root()
+	paramSet := assistant.NewParamSet[RootParameterSet](root)
+
+	paramSet.BindString(&assistant.FlagInfo{
+		Name:               configFlagName,
+		Usage:              configUsage,
+		Default:            defConfig,
+		AlternativeFlagSet: root.PersistentFlags(),
+	}, &paramSet.Native.ConfigFile)
+
 	const langFlagName = "lang"
 	const defLang = "en-GB"
 	const langUsage = "lang defines the language"
-	rootCommand.PersistentFlags().StringVar(bindToLang, langFlagName, defLang, langUsage)
+
+	paramSet.BindValidatedString(&assistant.FlagInfo{
+		Name:               langFlagName,
+		Usage:              langUsage,
+		Default:            defLang,
+		AlternativeFlagSet: root.PersistentFlags(),
+	}, &paramSet.Native.Language, func(value string) error {
+		_, err := language.Parse(value)
+		return err
+	})
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	const toggleFlagName = "toggle"
 	const toggleShort = "t"
-	const toggleUsage = "Help message for toggle"
-	rootCommand.Flags().BoolP(toggleFlagName, toggleShort, false, toggleUsage)
-}
+	const toggleUsage = "toggle Help message for toggle"
 
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
+	paramSet.BindBool(
+		assistant.NewFlagInfo(toggleUsage, toggleShort, false),
+		&paramSet.Native.Toggle,
+	)
 
-		// Search config in home directory with name ".main" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".main")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
+	container.MustRegisterParamSet("root-ps", paramSet)
 }
