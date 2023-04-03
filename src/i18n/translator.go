221523a3d45b1d0631e91e15233e2c3e57e478d3
plastikfan <plastikfan@outlook.com>
2023-04-03 11:33:55 +0100

    feat(i18n): optimise use function (#104)

--- src/i18n/translator.go
+++ src/i18n/translator.go
@@ -1,32 +1,19 @@
 package i18n
 
 import (
-	"github.com/samber/lo"
 	xi18n "github.com/snivilised/extendio/i18n"
-	"github.com/snivilised/extendio/xfs/utils"
-	"golang.org/x/text/language"
 )
 
 var (
 	// DefaultLanguage, update this with the required default
-	DefaultLanguage = utils.NewRoProp(language.BritishEnglish)
-	tx              xi18n.Translator
-	TxRef           utils.RoProp[xi18n.Translator] = utils.NewRoProp(tx)
+	DefaultLanguage = xi18n.DefaultLanguage
 )
-
-// containsLanguage should be exported by extendio, so it can be reused
-// instead of re-defining it here
-func containsLanguage(languages xi18n.SupportedLanguages, tag language.Tag) bool {
-	return lo.ContainsBy(languages, func(t language.Tag) bool {
-		return t == tag
-	})
-}
 
 // Text is the function to use to obtain a string created from
 // registered Localizers. The data parameter must be a go template
 // defining the input parameters and the translatable message content.
 func Text(data xi18n.Localisable) string {
-	return tx.Localise(data)
+	return xi18n.Text(data)
 }
 
 // Use, must be called by the client before any string data
@@ -37,65 +24,5 @@ func Text(data xi18n.Localisable) string {
 // the client must provide the App and Path properties indicating
 // how the l18n bundle is created.
 func Use(options ...xi18n.UseOptionFn) error {
-
-	o := &xi18n.UseOptions{}
-	for _, fo := range options {
-		fo(o)
-	}
-
-	// we let extendio manage translations for itself ...
-	//
-	err := xi18n.Use(func(uo *xi18n.UseOptions) {
-		uo.Tag = o.Tag
-		uo.From = o.From
-	})
-
-	if err != nil {
-		return err
-	}
-
-	// The root command has specified the translation requirements
-	// so we just use what has been requested.
-	//
-	lang := xi18n.NewLanguageInfo(o)
-	if !containsLanguage(lang.Supported, o.Tag) {
-		return xi18n.NewLanguageNotAvailableNativeError(o.Tag)
-	}
-
-	factory := lo.TernaryF(len(o.From.Sources) > 1,
-		func() xi18n.TranslatorFactory {
-			return &xi18n.MultiTranslatorFactory{}
-		},
-		func() xi18n.TranslatorFactory {
-			return &xi18n.SingularTranslatorFactory{}
-		},
-	)
-
-	tx = factory.New(lang)
-	TxRef = utils.NewRoProp(tx)
-
-	if TxRef.IsNone() {
-		return xi18n.NewFailedToCreateTranslatorNativeError(o.Tag)
-	}
-
-	return nil
-}
-
-// UseTx, do not use, required for unit testing only and is not considered
-// part of the public api and may be removed without corresponding version
-// number change.
-func UseTx(with xi18n.Translator, setters ...xi18n.UseOptionFn) error {
-	o := &xi18n.UseOptions{}
-	for _, fo := range setters {
-		fo(o)
-	}
-
-	tx = with
-	TxRef = utils.NewRoProp(tx)
-
-	if TxRef.IsNone() {
-		return xi18n.NewFailedToCreateTranslatorNativeError(o.Tag)
-	}
-
-	return nil
+	return xi18n.Use(options...)
 }
