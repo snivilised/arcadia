ce6cc9b6ae818b533ebf24b57768ebd389d6cfb7
plastikfan <plastikfan@outlook.com>
2023-04-22 12:32:09 +0100

    fix(command): make minor template project adjustments (#178)

--- src/i18n/messages-command.go
+++ src/i18n/messages-command.go
@@ -4,6 +4,7 @@ import (
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 )
 
+// 🧊 RootCmdShortDesc
 type RootCmdShortDescTemplData struct {
 	arcadiaTemplData
 }
@@ -16,6 +17,7 @@ func (td RootCmdShortDescTemplData) Message() *i18n.Message {
 	}
 }
 
+// 🧊 RootCmdLongDesc
 type RootCmdLongDescTemplData struct {
 	arcadiaTemplData
 }
@@ -33,6 +35,7 @@ func (td RootCmdLongDescTemplData) Message() *i18n.Message {
 	}
 }
 
+// 🧊 RootCmdConfigFileUsage
 type RootCmdConfigFileUsageTemplData struct {
 	arcadiaTemplData
 	ConfigFileName string
@@ -55,5 +58,32 @@ func (td RootCmdLangUsageTemplData) Message() *i18n.Message {
 		ID:          "root-command-language-usage",
 		Description: "root command lang usage",
 		Other:       "'lang' defines the language according to IETF BCP 47",
+	}
+}
+
+// 🧊 WidgetCmdShortDesc
+type WidgetCmdShortDescTemplData struct {
+	arcadiaTemplData
+}
+
+func (td WidgetCmdShortDescTemplData) Message() *i18n.Message {
+	return &i18n.Message{
+		ID:          "widget-command-short-description",
+		Description: "short description for the widget command",
+		Other:       "A brief description of widget command",
+	}
+}
+
+// 🧊 WidgetCmdLongDesc
+type WidgetCmdLongDescTemplData struct {
+	arcadiaTemplData
+}
+
+func (td WidgetCmdLongDescTemplData) Message() *i18n.Message {
+	return &i18n.Message{
+		ID:          "widget-command-long-description",
+		Description: "long description for the widget command",
+		Other: `A longer description that spans multiple lines and likely contains
+		examples and usage of using your application.`,
 	}
 }
